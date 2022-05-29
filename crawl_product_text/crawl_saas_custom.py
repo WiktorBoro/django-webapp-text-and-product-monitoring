4af29e49859e6e96f7478749218a6552b76a4b91
Wiktor Borowicz <105320026+WiktorBoro@users.noreply.github.com>
2022-05-29 17:17:13 +0200

    Corrected filtration product area, line 354

--- crawl_product_text/crawl_saas_custom.py
+++ crawl_product_text/crawl_saas_custom.py
@@ -351,12 +351,9 @@ class Crawl:
 
             # sometime if we enter id/class in custom option there are several area of the same id/class on page
             # so we need to find only this area where we get products
-            product_name_list = list()
-            for product_area in product_area_all:
-                product_name_list = product_area.find_all(html_code_symbol,
-                                                          attrs=arrt_regex_product_name)
-                if product_name_list:
-                    break
+            product_name_list = list(filter(lambda product_area: 
+                                            product_area.find_all(html_code_symbol, 
+                                                                  attrs=arrt_regex_product_name), product_area_all))
 
             self.product_name = ""
             self.amount_of_products = 0
